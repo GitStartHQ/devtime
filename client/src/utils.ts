fd16b80d84f7e445452613a887f3338ee1704de7
ztangaj <ztangaj@connect.ust.hk>
2021-06-17 11:34:38 +0800

    cannot access logDNA

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -1,5 +1,6 @@
 import moment from 'moment';
 import { convertDate } from './constants';
+import LogDNA from 'logdna';
 
 export const diffAndFormatShort = (beginDate, endDate) => {
     const diff = convertDate(endDate).diff(convertDate(beginDate));
@@ -8,17 +9,25 @@ export const diffAndFormatShort = (beginDate, endDate) => {
     return formattedDuration;
 };
 
+
+type LogDNAAppName = 
+  | 'devtime-client'
+  | 'devtime-electron';
+
 export const getLogDNAClient = (appName: LogDNAAppName) => {
-    const LOGDNA_API_KEY = getEnv('LOGDNA_API_KEY');
+    // const LOGDNA_API_KEY = getEnv('LOGDNA_API_KEY');
+    const LOGDNA_API_KEY = '33a02f1c20e286ca56534b48f0870447';
     const api_key = LOGDNA_API_KEY;
   
     if (!api_key) {
       console.log('LOGDNA_API_KEY key not found');
       return;
     }
+
+    const isProd = process.env.NODE_ENV === 'production';
   
     const options = {
-      app: isProd() ? `${appName}-prd-container` : `${appName}-stg-container`,
+      app: isProd ? `${appName}-prd-container` : `${appName}-stg-container`,
       env: 'local',
       tags: ['info', 'error', 'warning'],
     };
@@ -38,22 +47,23 @@ export const getLogDNAClient = (appName: LogDNAAppName) => {
     const _log = console.log;
     const _error = console.error;
   
-    const log = function () {
-      // eslint-disable-next-line prefer-rest-params
-      logger.log([...arguments].join(' '));
-      // eslint-disable-next-line prefer-rest-params
-      _log.apply(console, [...arguments]);
-    };
+    // const log = function () {
+    //   // eslint-disable-next-line prefer-rest-params
+    //   logger.log([...arguments].join(' '));
+    //   // eslint-disable-next-line prefer-rest-params
+    //   _log.apply(console, [...arguments]);
+    // };
   
-    const error = function () {
-      // eslint-disable-next-line prefer-rest-params
-      logger.error([...arguments].join(' '));
-      // eslint-disable-next-line prefer-rest-params
-      _error.apply(console, [...arguments]);
-    };
+    // const error = function () {
+    //   // eslint-disable-next-line prefer-rest-params
+    //   logger.error([...arguments].join(' '));
+    //   // eslint-disable-next-line prefer-rest-params
+    //   _error.apply(console, [...arguments]);
+    // };
   
-    console.log = log;
-    console.error = error;
+    // console.log = log;
+    // console.error = error;
+    logger.log('hello world');
   
     console.log('LogDNA Enabled');
   };
